708c45765cc9257c8aee4694e00fc05eb3fe2d1f
VenkataramanKarunakaran <Venkataraman.Karunakaran@cognizant.com>
2021-06-11 16:58:13 +0530

    Test Commit

--- FantasticView/FantasticView.swift
+++ FantasticView/FantasticView.swift
@@ -16,7 +16,7 @@ class FantasticView : UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in  //1
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in  //1
             UIView.animate(withDuration: 2.0) {  //2
                 self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
                 self.colorCounter+=1  //4
